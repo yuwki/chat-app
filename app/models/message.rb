93519c5f4eb3dd6578afad80f744b55f56af8836
yuwki <yuuki.ishii1208@gmail.com>
2020-07-25 11:52:33 +0900

    メッセージ送信機能の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :room
-  berongs_to :user
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "rooms#index"
+  root 'rooms#index'
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
